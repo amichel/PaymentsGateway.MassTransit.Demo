e958c136cba19a189eb0c90e5254bfa452192cf2
Alex Michel <amishel@gmail.com>
2015-10-12 17:57:32 +0300

    Working on the gateway FSM

---
+++ src/PaymentsGateway.Contracts/DepositValidationResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsGateway.Contracts
+{
+    public class DepositValidationResponse
+    {
+        public CcDepositRequest Request { get; set; }
+        public bool IsValid { get; set; }
+        public IEnumerable<ValidationResult> ValidationResults { get; set; }
+    }
+
+    public enum ValidationResult
+    {
+
+    }
+}
---
+++ src/PaymentsGateway.Gateway/IDepositValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using PaymentsGateway.Contracts;
+
+namespace PaymentsGateway.Gateway
+{
+    public interface IDepositValidator
+    {
+        void ValidateRequest(CcDepositRequest request, Action<DepositValidationResponse> onValidated);
+    }
+}
