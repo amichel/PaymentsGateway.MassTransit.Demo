4eecb56b9fb3c003cffe5197e538881d620842e0
Eigel Silviu <eigel.si@gmail.com>
2015-10-25 22:12:43 +0200

    improved layout to have the credit card container padded better

--- src/PaymentsGateway.WebUI/Scripts/components/payments/creditCardDeposit.js
+++ src/PaymentsGateway.WebUI/Scripts/components/payments/creditCardDeposit.js
@@ -3,34 +3,34 @@
 export default React.createClass({
 	render:function() {
 		return (
-			<div className="container-fluid bg-grey">
+			<div className="">
 				<h1>Have some fun by paying us with your: </h1>
-				<h2>Credit Card </h2>
-			  <form className="form">
+			  <div className="container-fluid row bg-orange bg-trasparent">
+				<h2 className="col-md-3">Credit Card</h2>
 
-				<div className="col-sm-7">
+				<div className="col-md-9">
 				  <div className="row">
-					<div className="col-sm-9 form-group">
+					<div className="col-md-9 form-group">
 						<input type="text" className="form-control" placeholder="Credit Card Number"/>
 					</div>
-					<div className="col-sm-3 form-group">
+					<div className="col-md-3 form-group">
 						<input type="text" className="form-control" placeholder="CVV"/>
 					</div>
-					<div className="col-sm-9 form-group">
+					<div className="col-md-9 form-group">
 					  <input type="text" className="form-control" placeholder="Name On The Card"/>
 					</div>
-					<div className="col-sm-3 form-group">
+					<div className="col-md-3 form-group">
 					  <input type="email" className="form-control" placeholder="Expiration Date"/>
 					</div>
 				  </div>
 				 
 				  <div className="row">
-					<div className="col-sm-12 form-group">
+					<div className="col-md-12 form-group">
 					  <button className="btn btn-default pull-right" type="submit">Pay</button>
 					</div>
 				  </div> 
 				</div>
-			  </form>
+			  </div>
 		</div>
 		);
 	}
