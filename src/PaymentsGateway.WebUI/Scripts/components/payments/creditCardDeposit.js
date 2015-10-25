11ea0cb69683e571aca26d8cd0cbc96329fa1da9
Eigel Silviu <eigel.si@gmail.com>
2015-10-25 21:29:09 +0200

    added page layout

--- src/PaymentsGateway.WebUI/Scripts/components/payments/creditCardDeposit.js
+++ src/PaymentsGateway.WebUI/Scripts/components/payments/creditCardDeposit.js
@@ -3,10 +3,35 @@
 export default React.createClass({
 	render:function() {
 		return (
-			<div>
-			<h1>Payment type:</h1>
-				<h2>Credit Card Number</h2>
-			</div>
+			<div className="container-fluid bg-grey">
+				<h1>Have some fun by paying us with your: </h1>
+				<h2>Credit Card </h2>
+			  <form className="form">
+
+				<div className="col-sm-7">
+				  <div className="row">
+					<div className="col-sm-9 form-group">
+						<input type="text" className="form-control" placeholder="Credit Card Number"/>
+					</div>
+					<div className="col-sm-3 form-group">
+						<input type="text" className="form-control" placeholder="CVV"/>
+					</div>
+					<div className="col-sm-9 form-group">
+					  <input type="text" className="form-control" placeholder="Name On The Card"/>
+					</div>
+					<div className="col-sm-3 form-group">
+					  <input type="email" className="form-control" placeholder="Expiration Date"/>
+					</div>
+				  </div>
+				 
+				  <div className="row">
+					<div className="col-sm-12 form-group">
+					  <button className="btn btn-default pull-right" type="submit">Pay</button>
+					</div>
+				  </div> 
+				</div>
+			  </form>
+		</div>
 		);
 	}
 })
