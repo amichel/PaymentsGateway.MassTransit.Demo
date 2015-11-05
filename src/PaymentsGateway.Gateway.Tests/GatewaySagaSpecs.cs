a0a9f55a9b6c2ac1569e1ab87fd6f0f5d896d8b5
Eigel Silviu <eigel.si@gmail.com>
2015-11-05 10:29:48 +0200

    added gatewaysaga first spec

---
+++ src/PaymentsGateway.Gateway.Tests/GatewaySagaSpecs.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Automatonymous;
+using MassTransit;
+using MassTransit.Saga;
+using MassTransit.TestFramework;
+using NSubstitute;
+using NUnit.Framework;
+using PaymentsGateway.Contracts;
+using PaymentsGateway.Gateway;
+using PaymentsGateway.Gateway.Components;
+
+namespace PaymentsGateway.GatewayService.Tests
+{
+    [TestFixture]
+    public class On_CcDepositRequest_Specs : InMemoryTestFixture
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _accountingLogicFacade.ClearReceivedCalls();
+        }
+
+        protected override void ConfigureInputQueueEndpoint(IReceiveEndpointConfigurator configurator)
+        {
+            _accountingLogicFacade = Substitute.For<IAccountingLogicFacade>();
+            var requestSettings = Substitute.For<RequestSettings>();
+            _machine = new GatewaySaga(new DepositResponseFactory(), new ClearingRequestFactory(), _accountingLogicFacade, requestSettings);
+            _repository = new InMemorySagaRepository<GatewaySagaState>();
+
+            configurator.StateMachineSaga(_machine, _repository);
+        }
+
+        private GatewaySaga _machine;
+        private InMemorySagaRepository<GatewaySagaState> _repository;
+        private IAccountingLogicFacade _accountingLogicFacade;
+
+        private static CcDepositRequest NewCcDepositRequest()
+        {
+            return new CcDepositRequest {AccountNumber = 111, CardId = 100};
+        }
+
+
+        [Test]
+        public async Task Moves_saga_to_AuthorizationFlow_Pending()
+        {
+            var newCcDepositRequest = NewCcDepositRequest();
+            var req = await Bus.Request(InputQueueAddress, newCcDepositRequest, x =>
+            {
+                x.Handle<CcDepositResponse>();
+                x.Timeout = TestTimeout;
+            }, TestCancellationToken);
+            await req.Task;
+
+            await _repository.ShouldContainSaga(x => Equals(x.CurrentState, _machine.AuthorizationFlow.Pending), TestTimeout);
+        }
+    }
+}
